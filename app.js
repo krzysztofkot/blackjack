a4ae97f2fffbc82a17904b594e6b27f9061d6701
Krzysztof Kot <kootek2@gmail.com>
2020-09-04 10:02:09 +0200

    create gameController function create UIController function create app controller function

---
+++ app.js
@@ -0,0 +1,205 @@
+//game controller
+const gameController = (function () {
+    //primary settings
+    const startCash = 1000;
+    totalCards = 8;
+    croupierMustPlayPoints = 16;
+    // players = [0, 1];
+
+    const data = {
+        totalCash: startCash,
+        bet: 0,
+        allCards: [],
+    };
+
+    class Card {
+        constructor(figure, color, id, value) {
+            this.figure = figure;
+            this.color = color;
+            this.id = id;
+            this.value = value;
+        }
+
+        checkAce() {
+            if (this.figure === "ace") {
+                return (this.isAce = true);
+            }
+        }
+    }
+
+    class Player {
+        constructor(name) {
+            this.name = name;
+            this.allCards = [];
+            this.totalPoints; // wymysleć jak to ma działać!! Ma sumować punkty kart z allCards
+        }
+    }
+
+    return {
+        updateData: (value, type) => (data[type] = parseInt(value)),
+
+        getData: () => data,
+
+        changeCash: () => (data.totalCash -= data.bet),
+
+        createPlayers: () => {
+            const person = new Player("person");
+            const croupier = new Player("croupier");
+        },
+
+        createCard: () => {
+            console.log("tworzenie karty");
+            generateCardRank();
+            //   getCardRank();
+            //   generateCardSuit();
+            //   createCardID();
+            //   uniqueID(); //stworzyć wszystkie powyższe funkcje
+        },
+
+        getRandomInt(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        },
+
+        generateCardRank: () => getRandomInt(2, 14),
+
+        getCardRank: () => {
+            const cardData = [];
+            const rank = generateCardRank();
+            if (rank <= 10) {
+                cardData.push(rank);
+                cardData.push(rank);
+            } else if (rank === 11) {
+                cardData.push("jack");
+                cardData.push(rank); //// wymysleć lepszą funkcję, ktora generuje kartę
+            }
+        },
+    };
+})();
+
+//UI controller
+
+const UIController = (function () {
+    const DOMstrings = {
+        betInput: ".bets__value--input",
+        totalCash: ".bets__value",
+        acceptBtn: ".bets__btn--accept",
+        allInBtn: ".bets__btn--all-in",
+    };
+    const DOMselectors = {
+        betInput: document.querySelector(DOMstrings.betInput),
+        totalCash: document.querySelector(DOMstrings.totalCash),
+        acceptBtn: document.querySelector(DOMstrings.acceptBtn),
+        allInBtn: document.querySelector(DOMstrings.allInBtn),
+    };
+
+    return {
+        insertBet: (e) => {
+            const val = e.target.value;
+            const len = val.length;
+
+            if (val.includes(".") || val.includes(" ") || isNaN(val)) {
+                const newVal = val.slice(0, len - 1);
+                e.target.value = newVal; // poprawić aby wyświetlalo '' jesli NaN ===true
+            }
+        },
+
+        returnBet: () => DOMselectors.betInput.value,
+
+        preventPaste: (e) => {
+            console.log("wklejanie!");
+            e.preventDefault();
+        },
+
+        addAllIn: () => {
+            const retriveCash = DOMselectors.totalCash.innerText;
+            DOMselectors.betInput.value = retriveCash;
+        },
+
+        printNewCash: (updateCash) =>
+            (DOMselectors.totalCash.innerText = updateCash),
+
+        inputToggleDisable: () => {
+            DOMselectors.betInput.disabled === true ?
+                (DOMselectors.betInput.disabled = false) :
+                (DOMselectors.betInput.disabled = true);
+        },
+
+        clearBetInput: () => {
+            DOMselectors.betInput.value = "";
+            DOMselectors.betInput.focus();
+        },
+
+        getDOMstrings: () => DOMstrings,
+    };
+})();
+
+//app controller
+
+const controller = (function (gameCtrl, UICtrl) {
+    const DOM = UICtrl.getDOMstrings();
+
+    //Setup event listeners for bets
+    const setupBetsEventListeners = () => {
+        document
+            .querySelector(DOM.betInput)
+            .addEventListener("keyup", UICtrl.insertBet);
+        document
+            .querySelector(DOM.betInput)
+            .addEventListener("paste", UICtrl.preventPaste);
+        document
+            .querySelector(DOM.allInBtn)
+            .addEventListener("click", UICtrl.addAllIn);
+        document.querySelector(DOM.acceptBtn).addEventListener("click", accept);
+    };
+
+    //Remove event listeners for bets
+
+    const removeBetsEventListener = () => {
+        document
+            .querySelector(DOM.betInput)
+            .removeEventListener("keyup", UICtrl.insertBet);
+        document
+            .querySelector(DOM.betInput)
+            .removeEventListener("paste", UICtrl.preventPaste);
+        document
+            .querySelector(DOM.allInBtn)
+            .removeEventListener("click", UICtrl.addAllIn);
+        document.querySelector(DOM.acceptBtn).removeEventListener("click", accept);
+    };
+
+    //accept bet
+    accept = () => {
+        //1. get current bet value
+        const value = UICtrl.returnBet();
+        //2. Update bet data
+        gameCtrl.updateData(value, "bet");
+        const currentData = gameCtrl.getData();
+
+        if (currentData.bet <= currentData.totalCash && currentData.bet > 0) {
+            //update player total cash
+            gameCtrl.changeCash();
+            const updatedData = gameCtrl.getData();
+            UICtrl.printNewCash(updatedData.totalCash);
+
+            //disable bet input
+            UICtrl.inputToggleDisable();
+            ////////////////////TODO change bets component color to grey /////////////////////////////////////////
+
+            //remove bet event listeners
+            removeBetsEventListener();
+            startGame();
+        } else {
+            UICtrl.clearBetInput();
+        }
+    };
+
+    const startGame = () => {
+        console.log("game has started");
+        gameCtrl.createPlayers();
+        gameCtrl.createCard(); //dodać argument
+    };
+
+    setupBetsEventListeners();
+})(gameController, UIController);
